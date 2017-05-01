115d812e31dae8d479033a00d9db11ffca64af89
Breno Machado <Breno Machado>
2017-05-01 15:28:59 -0300

    First test in Xamarin

---
+++ AppMoneys/AppMoneys/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using AppMoneys.ViewModels;
+using Xamarin.Forms;
+
+namespace AppMoneys
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ AppMoneys/AppMoneys/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace AppMoneys.ViewModels
+{
+    public class MainViewModel: BaseViewModel
+    {
+        private string _propriedadeTexto = "Texto Inicial";
+
+        public string PropriedadeTexto
+        {
+            get { return _propriedadeTexto; }
+            set
+            {
+                _propriedadeTexto = value;
+                OnPropertyChanged();
+            }
+        }
+
+    }
+}
